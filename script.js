f3198b6ef0136fa537d63ff79143cc9a58d776de
Francisco Dias de Bessa Jr <133795397+Chicaao@users.noreply.github.com>
2025-05-28 16:22:25 -0300

    Correção de erros na inserção do JS

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const hamburguerButton = document.querySelector('.hamburguer-menu')
     const navLinks = document.querySelector('.nav-links')
-    const navItems = document.querySelectorAll('nav-links li a')
+    const navItems = document.querySelectorAll('.nav-links li a')
 
     if (hamburguerButton && navLinks) {
         hamburguerButton.addEventListener('click', () => {
