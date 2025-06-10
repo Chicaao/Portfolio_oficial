8d59e8022b7e2f58fb2eb5a6e789e75b17466e13
Francisco Dias de Bessa Jr <133795397+Chicaao@users.noreply.github.com>
2025-06-10 14:51:19 -0300

    adiconando scroll suave

--- script.js
+++ script.js
@@ -13,5 +13,24 @@ document.addEventListener('DOMContentLoaded', () => {
                 navLinks.classList.remove('active');
             });
         });
+    } else {
+        console.warn("Erro: Elementos '.hamburguer-menu' ou '.nav-links' não encontrados no HTML.")
     }
+
+    // Adicionar Scroll Suave a todos os links internos
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            // Pega o ID da seção do atributo href (ex: "#about" -> "about")
+            const targetId = this.getAttribute('href').substring('');
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                targetElement.scrollIntoView({
+                    behavior: 'smooth'
+                });
+            }
+        });
+     });
 });
